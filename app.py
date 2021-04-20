973e648f0ec4dce5dede8a71f7e45f48dc2af28f
Ankan54 <eklapothik54@gmail.com>
2021-04-21 01:50:26 +0530

    Create app.py
    creating file to create a Flask application

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask_restful import Api
+from flask import Flask
+from flask_jwt import JWT
+
+from security import authenticate,identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store,StoreList
+from db import db
+
+app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False #stops the flask-sqlalchemy modification tracker as the same working in SQLAlchemy
+app.secret_key= 'ankan.bera'  #should not be visible to others while deploying
+api= Api(app)
+jwt= JWT(app, authenticate, identity)  #creates a new endpoint '/auth'
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+#instead of @app.route decorator
+api.add_resource(Item,'/item/<string:name>')
+api.add_resource(ItemList,'/items')
+api.add_resource(UserRegister,'/register')
+api.add_resource(Store,'/store/<string:name>')
+api.add_resource(StoreList,'/stores')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(host="localhost",port=5000,debug=True)

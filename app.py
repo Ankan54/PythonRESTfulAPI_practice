c2982999963939198ae9eb1ba10787a57d432cd8
Ankan54 <eklapothik54@gmail.com>
2021-04-24 20:24:42 +0530

    bug fixes regarding using postgre

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store,StoreList
+from db import db
 
 
 app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URI','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False #stops the flask-sqlalchemy modification tracker as the same working in SQLAlchemy
 app.secret_key= 'ankan.bera'  #should not be visible to others while deploying
 api= Api(app)
@@ -26,6 +27,5 @@ api.add_resource(Store,'/store/<string:name>')
 api.add_resource(StoreList,'/stores')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000,debug=True)

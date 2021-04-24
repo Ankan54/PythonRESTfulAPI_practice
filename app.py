cc77e66105bea0b49dce5418c4280b483fd48697
Ankan54 <eklapothik54@gmail.com>
2021-04-24 20:04:02 +0530

    changed position of import db

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from security import authenticate,identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store,StoreList
-from db import db
+
 
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
@@ -26,5 +26,6 @@ api.add_resource(Store,'/store/<string:name>')
 api.add_resource(StoreList,'/stores')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000,debug=True)
